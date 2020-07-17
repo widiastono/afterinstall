5efde60e646d956d70b2746c6f80c45be7e55e32
Wahyu Widiastono <widiastono@crx.solonet.net.id>
2020-07-18 06:42:16 +0700

    update install

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
+USER=widiastono
 
-### Installing Pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+### root vim
+sh vim.install.sh
 
-### Install Airline using pathogen
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-
-### Install Airline Theme
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-
-### Install NERDTree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-
-### Installing emmet
-git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle
-
-### Configure .vimrc
-mv  ~/.vimrc ~/.vimrc.old
-cp .vimrc ~/.vimrc
+### USER vim
+sudo - widiastono vim.install.sh
